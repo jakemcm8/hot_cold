ef504f61698517fb6056f6843285bf1c98e99712
Jake <jakem80808@gmail.com>
2023-03-25 02:54:26 -0700

    Added number comparison

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Try to guess the number!");
+    println!("Try to guess the number 1 - 100!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess");
-
     let mut guess = String::new();
+
+    println!("Please input your guess");
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line!");
 
+    let guess: u32 = guess.trim().parse().expect("Please type only numbers!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too large!"),
+        Ordering::Equal => println!("You got it!")
+}
+
+
     println!("You guessed: {guess}");
     println!("Actual number: {secret_number}")
+
+
 }
